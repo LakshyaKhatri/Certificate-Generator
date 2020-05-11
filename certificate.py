34e139071a565006f067c11a02ec28293a89fa09
LakshyaKhatri <lakshyak67@gmail.com>
2020-05-11 23:52:53 +0530

    Add commandline argument parsing

--- certificate.py
+++ certificate.py
@@ -1,29 +1,52 @@
-from PIL import Image, ImageDraw, ImageFont
-import json
+# from PIL import Image, ImageDraw, ImageFont
+# import json
+#
+# template = "1"
+# config = ""
+# with open('template-config.json') as configFile:
+#     config = json.load(configFile)[template]
+#
+# certificate = Image.open('templates/' + template + '.jpg')
+# drawHandler = ImageDraw.Draw(certificate)
+# values = ['Lakshya Khatri', 'Data Science', 'Lakshya Khatri', '10 / May / 2020', 'LAKSHYA KHATRI']
+#
+# i = 0
+# for field in config:
+#     if field['type'] == 'text':
+#         fieldValue = values[i]
+#         font = ImageFont.truetype('fonts/' + field['font'], field['size'])
+#         textWidth, textHeight = drawHandler.textsize(fieldValue, font=font)
+#         drawHandler.text((field['x'] - (textWidth // 2), field['y'] - (textHeight // 2)),
+#                          fieldValue, fill=field['color'], font=font)
+#         i += 1
+#
+#     elif field['type'] == 'image':
+#         image = Image.open(field['path']).convert('RGBA')
+#         imageWidth, imageHeight = image.size
+#         certificate.paste(image, (field['x'] - (imageWidth // 2),
+#                                   field['y'] - (imageHeight // 2)), image)
+#
+# certificate.save('../certificates/' + values[0] + '.pdf')
 
-template = "1"
-config = ""
-with open('template-config.json') as configFile:
-    config = json.load(configFile)[template]
+import argparse
 
-certificate = Image.open('templates/' + template + '.jpg')
-drawHandler = ImageDraw.Draw(certificate)
-values = ['Lakshya Khatri', 'Data Science', 'Lakshya Khatri', '10 / May / 2020', 'LAKSHYA KHATRI']
 
-i = 0
-for field in config:
-    if field['type'] == 'text':
-        fieldValue = values[i]
-        font = ImageFont.truetype('fonts/' + field['font'], field['size'])
-        textWidth, textHeight = drawHandler.textsize(fieldValue, font=font)
-        drawHandler.text((field['x'] - (textWidth // 2), field['y'] - (textHeight // 2)),
-                         fieldValue, fill=field['color'], font=font)
-        i += 1
+def certify():
+    pass
 
-    elif field['type'] == 'image':
-        image = Image.open(field['path']).convert('RGBA')
-        imageWidth, imageHeight = image.size
-        certificate.paste(image, (field['x'] - (imageWidth // 2),
-                                  field['y'] - (imageHeight // 2)), image)
 
-certificate.save('../certificates/' + values[0] + '.pdf')
+def main():
+    parser = argparse.ArgumentParser(
+        prog='certify',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument('template', type=int, help='template to use')
+    parser.add_argument('-i', '--input', help='name of the input CSV file',
+                        default=False)
+    parser.add_argument('-o', '--output', help='output directory',
+                        default='./')
+    certify(parser.parse_args())
+
+
+if __name__ == '__main__':
+    main()
