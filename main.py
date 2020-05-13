ef5acf957110769fe36f6c9e4fd85165cd07ecd3
LakshyaKhatri <lakshyak67@gmail.com>
2020-05-13 17:45:43 +0530

    Minor changes

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument('template', help='template to use')
+    parser.add_argument('-i', '--input', help='name of the input CSV file',
+                        default=False)
     args = parser.parse_args()
 
     # This object will handle certificate generation
-    certify = Certify(args.template, args.input, args.output)
+    certify = Certify(args.template, args.input)
     while certify.has_next_input():
         certify.next_input()
         certify.generate_certificate()
+
+
+if __name__ == '__main__':
+    main()
